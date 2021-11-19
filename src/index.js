5393116b7d141aabf1c63a7ea46e8afe297550cd
Alexey Sapr <alexeysapr@gmail.com>
2021-11-19 11:32:44 +0200

    Update index and styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import './sass/main.scss';
 
-import debounce from 'lodash.debounce';
-import card from './templates/film-card-template.hbs';
-import apiService from './js/apiService.js';
+import './js/film-container.js';
---
+++ src/js/film-container.js
@@ -0,0 +1,3 @@
+import debounce from 'lodash.debounce';
+import card from '../templates/film-card-template.hbs';
+import apiService from './apiService.js';
