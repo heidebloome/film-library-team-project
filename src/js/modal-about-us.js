bd2270017c5ee2518dcba3671176a5cd794e8f18
Sergii <83786081+MasenkoSS@users.noreply.github.com>
2021-11-30 19:05:24 +0300

    closed ecs and overlay

--- src/js/modal-about-us.js
+++ src/js/modal-about-us.js
@@ -1,14 +1,32 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    overlayClick: document.querySelector('.backdrop_command')
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  refs.overlayClick.addEventListener('click', onOverlayClick);
+
+  function onOverlayClick() {
+    toggleModal();
+  }
+
+  window.addEventListener('keydown', onOverlaykey);
+
+  function onOverlaykey(evt) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = evt.code === ESC_KEY_CODE;
+
+    if (isEscKey) {
+      toggleModal();
     }
-  })();
+  }
+})();
